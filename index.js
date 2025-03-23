a130a499ba0ad617fa792b79469a34fe49aae75d
Michael Ouma <Oumamichael108@gmail.com>
2025-03-23 21:34:09 +0300

    Updating my time-card and payroll application3

--- index.js
+++ index.js
@@ -1,139 +1,4 @@
-
-/**
- * createEmployeeRecord
- * @param {Array} arr - [firstName, familyName, title, payPerHour]
- * @returns {Object} - Employee record object
- */
-function createEmployeeRecord(arr) {
-    return {
-      firstName: arr[0],
-      familyName: arr[1],
-      title: arr[2],
-      payPerHour: arr[3],
-      timeInEvents: [],
-      timeOutEvents: []
-    };
-  }
-  
-  /**
-   * createEmployeeRecords
-   * @param {Array} arrs - Array of arrays, each representing an employee record
-   * @returns {Array} - Array of employee record objects
-   */
-  function createEmployeeRecords(arrs) {
-    return arrs.map(record => createEmployeeRecord(record));
-  }
-  
-  /**
-   * createTimeInEvent
-   * @param {Object} record - Employee record object
-   * @param {String} dateStamp - "YYYY-MM-DD HHMM"
-   * @returns {Object} - Updated employee record object
-   */
-  function createTimeInEvent(record, dateStamp) {
-    const [date, hour] = dateStamp.split(" ");
-    record.timeInEvents.push({
-      type: "TimeIn",
-      hour: parseInt(hour, 10),
-      date: date
-    });
-    return record;
-  }
-  
-  /**
-   * createTimeOutEvent
-   * @param {Object} record - Employee record object
-   * @param {String} dateStamp - "YYYY-MM-DD HHMM"
-   * @returns {Object} - Updated employee record object
-   */
-  function createTimeOutEvent(record, dateStamp) {
-    const [date, hour] = dateStamp.split(" ");
-    record.timeOutEvents.push({
-      type: "TimeOut",
-      hour: parseInt(hour, 10),
-      date: date
-    });
-    return record;
-  }
-  
-  /**
-   * hoursWorkedOnDate
-   * @param {Object} record - Employee record object
-   * @param {String} targetDate - "YYYY-MM-DD"
-   * @returns {Number} - Hours worked (integer)
-   */
-  function hoursWorkedOnDate(record, targetDate) {
-    const timeIn = record.timeInEvents.find(event => event.date === targetDate);
-    const timeOut = record.timeOutEvents.find(event => event.date === targetDate);
-    // Assumes hours are in HHMM format on the hour so dividing by 100 gives whole hours
-    return (timeOut.hour - timeIn.hour) / 100;
-  }
-  
-  /**
-   * wagesEarnedOnDate
-   * @param {Object} record - Employee record object
-   * @param {String} targetDate - "YYYY-MM-DD"
-   * @returns {Number} - Wages earned for the day
-   */
-  function wagesEarnedOnDate(record, targetDate) {
-    const hours = hoursWorkedOnDate(record, targetDate);
-    return hours * record.payPerHour;
-  }
-  
-  /**
-   * allWagesFor
-   * Uses the employee record as context (this)
-   * @returns {Number} - Total wages for all dates
-   *
-   * NOTE: This implementation uses .bind(this) to avoid a well-known lost context bug.
-   
-  const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-      return e.date;
-    });
-  
-    const payable = eligibleDates.reduce(function (memo, d) {
-      // Use call to set the context correctly for wagesEarnedOnDate
-      return memo + wagesEarnedOnDate.call(this, d);
-    }.bind(this), 0);
-  
-    return payable;
-};*/
-  
-  /**
-   * findEmployeeByFirstName
-   * @param {Array} srcArray - Array of employee records
-   * @param {String} firstName - The first name to search for
-   * @returns {Object|undefined} - Matching employee record or undefined if not found
-   */
-  function findEmployeeByFirstName(srcArray, firstName) {
-    return srcArray.find(record => record.firstName === firstName);
-  }
-  
-  /**
-   * calculatePayroll
-   * @param {Array} employeeRecords - Array of employee records
-   * @returns {Number} - Total payroll for all employees
-   */
-  function calculatePayroll(employeeRecords) {
-    return employeeRecords.reduce((total, record) => {
-      return total + allWagesFor.call(record);
-    }, 0);
-  }
-  
-  module.exports = {
-    createEmployeeRecord,
-    createEmployeeRecords,
-    createTimeInEvent,
-    createTimeOutEvent,
-    hoursWorkedOnDate,
-    wagesEarnedOnDate,
-    allWagesFor,
-    findEmployeeByFirstName,
-    calculatePayroll
-  };
-  
-  
+/* Your Code Here */
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -143,6 +8,67 @@ function createEmployeeRecord(arr) {
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords(array) {
+    return array.map(function (e) {
+        return createEmployeeRecord(e)
+    })
+}
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+function hoursWorkedOnDate(date) {
+    let inEvent = this.timeInEvents.find(function (e) {
+        return e.date === date
+    })
+
+    let outEvent = this.timeOutEvents.find(function (e) {
+        return e.date === date
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+function wagesEarnedOnDate(date) {  
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(function (rec) {
+        return rec.firstName === firstName
+    })
+}
+function calculatePayroll(array) {
+    return array.reduce(function (memo, rec) {
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -155,4 +81,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
